7e5193634ddd26911ee259cde4e944a7f27bd3ba
Stefan Simon <stefan.j.c.simon@gmail.com>
2025-02-07 06:26:51 +0100

    [+] Success and error handler

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
+
 use std::sync::Arc;
 
-use actix_web::{get, web::Data, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web::{Data, Json}, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
 
+// TODO: needs to be async
 trait LoadUserService: Send + Sync {
     type User;
 
-    fn load_user(&self, username: &str, password: &str) -> Self::User;
+    fn load_user(&self, username: &str, password: &str) -> Result<Self::User, ()>;
+
+    fn on_success_handler(&self, req: &HttpRequest, user: &Self::User);
+    fn on_error_handler(&self, req: &HttpRequest);
 }
 
 struct MyUser {
@@ -17,17 +23,45 @@ struct HardCodedLoadUserService {}
 impl LoadUserService for HardCodedLoadUserService {
     type User = MyUser;
 
-    fn load_user(&self, _username: &str, _password: &str) -> Self::User {
-        MyUser {
-            name: "Dummy".to_owned(),
+    fn load_user(&self, username: &str, password: &str) -> Result<Self::User, ()> {
+        if username == "test" && password == "test123" {
+            Ok(MyUser {
+                name: "Dummy".to_owned(),
+            })
+        } else {
+            Err(())
         }
+        
     }
+    
+    fn on_success_handler(&self, _req: &HttpRequest, _user: &Self::User) {
+        println!("Login successfully");
+    }
+    
+    fn on_error_handler(&self, _req: &HttpRequest) {
+        println!("Login failed!");
+    }
+}
+
+#[derive(Serialize,Deserialize)]
+pub struct LoginToken {
+    pub username: String,
+    pub password: String,
 }
 
 
 #[get("/login")]
-async fn login(_user_service: Data<Arc<dyn LoadUserService<User = MyUser>>>) -> impl Responder {
-    HttpResponse::Ok()
+async fn login(login_token: Json<LoginToken>, user_service: Data<Arc<dyn LoadUserService<User = MyUser>>>, req: HttpRequest) -> impl Responder {
+    match user_service.load_user(&login_token.username, &login_token.password) {
+        Ok(u) => {
+            user_service.on_success_handler(&req, &u);
+            HttpResponse::Ok()
+        },
+        Err(_) => {
+            user_service.on_error_handler(&req);
+            HttpResponse::BadRequest()
+        },
+    }
 }
 
 
