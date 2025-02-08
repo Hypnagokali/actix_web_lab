c0033ab20f602be3965a21ebb2476b074ead1417
Stefan Simon <stefan.j.c.simon@gmail.com>
2025-02-08 17:06:47 +0100

    [*] Async methods for LoadUserService and proper error type

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 
-use std::sync::Arc;
+use std::{future::ready, sync::Arc};
 
-use actix_web::{get, web::{Data, Json}, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{post, web::{Data, Json}, App, Error, HttpRequest, HttpResponse, HttpServer, Responder, ResponseError};
+use futures::future::LocalBoxFuture;
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
 
-// TODO: needs to be async
 trait LoadUserService: Send + Sync {
     type User;
 
-    fn load_user(&self, username: &str, password: &str) -> Result<Self::User, ()>;
+    fn load_user(&self, username: &str, password: &str) -> LocalBoxFuture<'_, Result<Self::User, LoadUserError>>;
 
-    fn on_success_handler(&self, req: &HttpRequest, user: &Self::User);
-    fn on_error_handler(&self, req: &HttpRequest);
+    fn on_success_handler(&self, req: &HttpRequest, user: &Self::User) -> LocalBoxFuture<'_, Result<(), LoadUserError>>;
+    fn on_error_handler(&self, req: &HttpRequest) -> LocalBoxFuture<'_, Result<(), LoadUserError>>;
 }
 
 struct MyUser {
@@ -23,23 +24,56 @@ struct HardCodedLoadUserService {}
 impl LoadUserService for HardCodedLoadUserService {
     type User = MyUser;
 
-    fn load_user(&self, username: &str, password: &str) -> Result<Self::User, ()> {
+    fn load_user(&self, username: &str, password: &str) -> LocalBoxFuture<'_, Result<Self::User, LoadUserError>> {
         if username == "test" && password == "test123" {
-            Ok(MyUser {
-                name: "Dummy".to_owned(),
-            })
+            Box::pin(ready(Ok(
+                MyUser {
+                        name: "Dummy".to_owned(),
+                }
+            ))
+            )
         } else {
-            Err(())
+            Box::pin(async {
+                Err(LoadUserError::LoginFailed)
+            })
         }
         
     }
     
-    fn on_success_handler(&self, _req: &HttpRequest, _user: &Self::User) {
-        println!("Login successfully");
+    fn on_success_handler(&self, _req: &HttpRequest, _user: &Self::User) -> LocalBoxFuture<'_, Result<(), LoadUserError>> {
+        if true {
+            println!("Generate error while in success handler");
+            let m = LoadUserError::HandlerError("Something went wrong".to_owned());
+            Box::pin(ready(Err(m)))
+        } else {
+            println!("Login successfully");
+            Box::pin(ready(Ok(())))
+        }
     }
     
-    fn on_error_handler(&self, _req: &HttpRequest) {
-        println!("Login failed!");
+    fn on_error_handler(&self, _req: &HttpRequest) -> LocalBoxFuture<'_, Result<(), LoadUserError>> {
+        println!("Login failed");
+        Box::pin(ready(Ok(())))
+    }
+}
+
+#[derive(Error, Debug)]
+enum LoadUserError {
+    #[error("Username or password wrong")]
+    LoginFailed,
+    #[error("Error in an handler function: {0}")]
+    HandlerError(String),
+}
+
+impl ResponseError for LoadUserError {
+    fn error_response(&self) -> HttpResponse {      
+        match self {
+            LoadUserError::LoginFailed => {
+                println!("Return 401 Error");
+                HttpResponse::Unauthorized().body(self.to_string())
+            },
+            LoadUserError::HandlerError(_) => HttpResponse::InternalServerError().body(self.to_string()),
+        }
     }
 }
 
@@ -50,16 +84,16 @@ pub struct LoginToken {
 }
 
 
-#[get("/login")]
-async fn login(login_token: Json<LoginToken>, user_service: Data<Arc<dyn LoadUserService<User = MyUser>>>, req: HttpRequest) -> impl Responder {
-    match user_service.load_user(&login_token.username, &login_token.password) {
+#[post("/login")]
+async fn login(login_token: Json<LoginToken>, user_service: Data<Arc<dyn LoadUserService<User = MyUser>>>, req: HttpRequest) -> Result<impl Responder, Error> {
+    match user_service.load_user(&login_token.username, &login_token.password).await {
         Ok(u) => {
-            user_service.on_success_handler(&req, &u);
-            HttpResponse::Ok()
+            user_service.on_success_handler(&req, &u).await?;
+            Ok(HttpResponse::Ok())
         },
-        Err(_) => {
-            user_service.on_error_handler(&req);
-            HttpResponse::BadRequest()
+        Err(e) => {
+            user_service.on_error_handler(&req).await?;
+            Err(e.into())
         },
     }
 }
