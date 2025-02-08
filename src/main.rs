841449a6a40461b4212414f6a35216620dffa3be
Stefan Simon <stefan.j.c.simon@gmail.com>
2025-02-08 18:10:17 +0100

    [+] Introducing a wrapper to simplify handling

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,25 @@
 
-use std::{future::ready, sync::Arc};
+use std::{future::ready, ops::Deref, sync::Arc};
 
 use actix_web::{post, web::{Data, Json}, App, Error, HttpRequest, HttpResponse, HttpServer, Responder, ResponseError};
 use futures::future::LocalBoxFuture;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
-trait LoadUserService: Send + Sync {
+pub trait LoadUserService: Send + Sync {
     type User;
 
     fn load_user(&self, username: &str, password: &str) -> LocalBoxFuture<'_, Result<Self::User, LoadUserError>>;
-
     fn on_success_handler(&self, req: &HttpRequest, user: &Self::User) -> LocalBoxFuture<'_, Result<(), LoadUserError>>;
     fn on_error_handler(&self, req: &HttpRequest) -> LocalBoxFuture<'_, Result<(), LoadUserError>>;
 }
 
-struct MyUser {
+#[allow(dead_code)]
+pub struct MyUser {
     pub name: String,
 }
 
-struct HardCodedLoadUserService {}
+pub struct HardCodedLoadUserService {}
 
 impl LoadUserService for HardCodedLoadUserService {
     type User = MyUser;
@@ -30,25 +30,17 @@ impl LoadUserService for HardCodedLoadUserService {
                 MyUser {
                         name: "Dummy".to_owned(),
                 }
-            ))
-            )
+            )))
         } else {
             Box::pin(async {
                 Err(LoadUserError::LoginFailed)
             })
         }
-        
     }
     
     fn on_success_handler(&self, _req: &HttpRequest, _user: &Self::User) -> LocalBoxFuture<'_, Result<(), LoadUserError>> {
-        if true {
-            println!("Generate error while in success handler");
-            let m = LoadUserError::HandlerError("Something went wrong".to_owned());
-            Box::pin(ready(Err(m)))
-        } else {
-            println!("Login successfully");
-            Box::pin(ready(Ok(())))
-        }
+        println!("Login successful");
+        Box::pin(ready(Ok(())))
     }
     
     fn on_error_handler(&self, _req: &HttpRequest) -> LocalBoxFuture<'_, Result<(), LoadUserError>> {
@@ -58,7 +50,8 @@ impl LoadUserService for HardCodedLoadUserService {
 }
 
 #[derive(Error, Debug)]
-enum LoadUserError {
+#[allow(dead_code)]
+pub enum LoadUserError {
     #[error("Username or password wrong")]
     LoginFailed,
     #[error("Error in an handler function: {0}")]
@@ -83,9 +76,32 @@ pub struct LoginToken {
     pub password: String,
 }
 
+// TODO: find another name
+pub struct LoadUserServiceTrait<U> (Arc<dyn LoadUserService<User = U>>);
+
+impl<U> Clone for LoadUserServiceTrait<U> {
+    fn clone(&self) -> Self {
+        Self(self.0.clone())
+    }
+}
+
+impl<U> LoadUserServiceTrait<U> {
+    pub fn new(us: impl LoadUserService<User = U> +'static) -> Self {
+        Self(Arc::new(us))
+    }
+}
+
+
+impl<U> Deref for LoadUserServiceTrait<U> {
+    type Target = Arc<dyn LoadUserService<User = U>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
 #[post("/login")]
-async fn login(login_token: Json<LoginToken>, user_service: Data<Arc<dyn LoadUserService<User = MyUser>>>, req: HttpRequest) -> Result<impl Responder, Error> {
+async fn login(login_token: Json<LoginToken>, user_service: Data<LoadUserServiceTrait<MyUser>>, req: HttpRequest) -> Result<impl Responder, Error> {
     match user_service.load_user(&login_token.username, &login_token.password).await {
         Ok(u) => {
             user_service.on_success_handler(&req, &u).await?;
@@ -103,8 +119,9 @@ async fn login(login_token: Json<LoginToken>, user_service: Data<Arc<dyn LoadUse
 async fn main() -> std::io::Result<()>{
     std::env::set_var("RUST_LOG", "debug");
     env_logger::init();
-    let user_service: Arc<dyn LoadUserService<User = MyUser>> = Arc::new(HardCodedLoadUserService {});
+    // let user_service: Arc<dyn LoadUserService<User = MyUser>> = Arc::new(HardCodedLoadUserService {});
 
+    let user_service = LoadUserServiceTrait::new(HardCodedLoadUserService {});
     HttpServer::new(move || {
         App::new()
             .service(login)
